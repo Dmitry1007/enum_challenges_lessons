8bb4bde58cf85038b8b315226a0b41900e216dcb
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-05-01 12:20:16 -0600

    reduces arry of elements into hash with frequency as value

--- test/reduce_test.rb
+++ test/reduce_test.rb
@@ -1,7 +1,7 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/spec'
-require_relative '../lib/reducer.rb'
+require_relative '../lib/reducerator.rb'
 
 describe Reducer do
   it "can reduce to a hash of how many times the number occurs in the array." do
@@ -12,15 +12,19 @@ describe Reducer do
 
   it "can reduce an array of letters to a hash of letter frequency" do
     letter_array = ["a", "b", "c", "c", "d", "d", "d"]
+    reducer= Reducer.new(letter_array)
+    assert_equal ({"a" => 1, "b" => 1, "c" => 2, "d" => 3}), reducer.reduce
   end
 
   it "can reverse a sentence with reduce" do
+    skip
     sentence = "I am a unicorn"
     reducer = Reducer.new(sentence)
     assert_equal "nrocinu a ma I", reducer.sent_reverse
   end
 
   it "can reverse the word order in a sentence" do
+    skip
     sentence = "I am a unicorn"
     reducer = Reducer.new(sentence)
     assert_equal "unicorn a am I", reducer.word_reverse
